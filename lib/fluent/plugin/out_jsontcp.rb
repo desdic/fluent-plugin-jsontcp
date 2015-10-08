0fe273529275b0ff7e580dd82d071e89d3693e8d
Kim Nielsen <kgn@one.com>
2015-10-08 10:18:32 +0200

    Switched to buffered output

--- lib/fluent/plugin/out_jsontcp.rb
+++ lib/fluent/plugin/out_jsontcp.rb
@@ -1,71 +1,69 @@
 
 module Fluent
-  class JSONTCPOutput < TimeSlicedOutput
+    class JSONTCPOutput < ObjectBufferedOutput
 
-    Plugin.register_output('jsontcp', self)
+        Plugin.register_output('jsontcp', self)
 
-    config_param :path, :string
-    config_param :format, :string, :default => 'json'
-    config_param :port,   :integer, :default => 3540
-    config_param :host,   :string, :default => 'localhost'
+        OUTPUT_PROCS = {
+            :json => Proc.new {|record| Yajl.dump(record) },
+            :hash => Proc.new {|record| record.to_s },
+        }
 
-    def initialize
-      require 'time'
-      require 'socket'
-      super
+        config_param :port,   :integer, :default => 3540
+        config_param :host,   :string, :default => 'localhost'
+
+        config_param :format, :default => :json do |val|
+            case val.downcase
+            when 'json'
+                :json
+            when 'hash'
+                :hash
+            else
+                raise ConfigError, "bufferd stdout output format should be 'json' or 'hash'"
+            end
+        end
+
+        def initialize
+            require 'time'
+            require 'socket'
+            super
+        end
+
+        def connect()
+            TCPSocket.new(@host, @port)
+        end
+
+        def configure(conf)
+            unless conf['buffer_path']
+                raise ConfigError, "'buffer_path' parameter is required on output"
+            end
+
+            super
+
+            @output_proc = OUTPUT_PROCS[@format]
+        end
+
+        def write_objects(tag, es)
+            sock = connect()
+            begin
+                content = ''
+                es.each {|time,record|
+                    content << "#{@output_proc.call(record)}"
+                }
+                sock.puts content
+                return
+            ensure
+                sock.close
+            end
+
+            return false
+        end
+
+        def secondary_init(primary)
+            # don't warn even if primary.class is not FileOutput
+        end
+
+        private
+
     end
-
-    def connect()
-      TCPSocket.new(@host, @port)
-    end
-
-    def configure(conf)
-      if path = conf['path']
-        @path = path
-      end
-      unless @path
-        raise ConfigError, "'path' parameter is required on output"
-      end
-
-      if pos = @path.index('*')
-        @path_prefix = @path[0,pos]
-        @path_suffix = @path[pos+1..-1]
-        conf['buffer_path'] ||= "#{@path}"
-      else
-        @path_prefix = @path+"."
-        @path_suffix = ".log"
-        conf['buffer_path'] ||= "#{@path}.*"
-      end
-
-      super
-
-      @formatter = Plugin.new_formatter(@format)
-      @formatter.configure(conf)
-
-      @buffer.symlink_path = @symlink_path if @symlink_path
-    end
-
-    def format(tag, time, record)
-      @formatter.format(tag, time, record)
-    end
-
-    def write(chunk)
-	sock = connect()
-	begin
-		chunk.write_to(sock)
-		return
-	ensure
-		sock.close
-	end
-
-      return false
-    end
-
-    def secondary_init(primary)
-      # don't warn even if primary.class is not FileOutput
-    end
-
-    private
-
-  end
 end
